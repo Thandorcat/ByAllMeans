6da511fbdf21fe993e1f564abdd93d95ede57700
Vlad Osipov <osipov.06@mail.ru>
2017-04-28 14:59:32 +0300

    EmployeerManage JUnit tests refactoring

--- app/src/test/java/com/anyway/byallmeans/EmployeeManagerTest.java
+++ app/src/test/java/com/anyway/byallmeans/EmployeeManagerTest.java
@@ -1,15 +1,9 @@
 package com.anyway.byallmeans;
 
-import junit.framework.Assert;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.experimental.theories.suppliers.TestedOn;
 
-import static junit.framework.Assert.assertNull;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
 
 public class EmployeeManagerTest {
 
@@ -23,70 +17,137 @@ public class EmployeeManagerTest {
     }
 
     @Test
-    public void addEmployeeTest() {
+    public void addEmployee_NewEmployee_AllEmployeesListHasOneElement() {
         testEmployeeManager = new EmployeeManager();
         testEmployeeManager.add(testEmployee);
-        assertNotNull(testEmployeeManager.getAllEmployees());
-
-        assertNotNull(testEmployeeManager.getAvailableEmployees());
-        assertNotNull(testEmployeeManager.getAvailableEmployeesData());
-
-        assertNotNull(testEmployeeManager.getUnavailableEmployees());
-        assertNotNull(testEmployeeManager.getUnavailableEmployeesData());
-    }
-
+        assertEquals(1, testEmployeeManager.getAllEmployees().size());
+  }
 
     @Test
-    public void sendToTraining() {
+    public void addEmployee_NewEmployee_AvailableEmployeesListHasOneElement() {
+        testEmployeeManager = new EmployeeManager();
+        testEmployeeManager.add(testEmployee);
+
+        assertEquals(1, testEmployeeManager.getAvailableEmployees().size());
+    }
+
+    @Test
+    public void addEmployee_NewEmployee_AvailableEmployeesDataListHasOneElement() {
+        testEmployeeManager = new EmployeeManager();
+        testEmployeeManager.add(testEmployee);
+
+        assertEquals(1, testEmployeeManager.getAvailableEmployeesData().size());
+    }
+
+    @Test
+    public void addEmployee_NewEmployee_UnavailableEmployeesListHasNoElements() {
+        testEmployeeManager = new EmployeeManager();
+        testEmployeeManager.add(testEmployee);
+
+        assertEquals(0, testEmployeeManager.getUnavailableEmployees().size());
+    }
+
+    @Test
+    public void addEmployee_NewEmployee_UnavailableEmployeesDataListHasNoElements() {
+        testEmployeeManager = new EmployeeManager();
+        testEmployeeManager.add(testEmployee);
+
+        assertEquals(0, testEmployeeManager.getUnavailableEmployeesData().size());
+    }
+
+    //remove hardcode
+    @Test
+    public void sendToTraining_IndexInBounds_AvailableEmployeesListHasNoElements() {
         testEmployeeManager = new EmployeeManager();
         testEmployeeManager.add(testEmployee);
         testEmployeeManager.sendToTraining(0);
         testEmployee = testEmployeeManager.getUnavailableEmployees().get(0);
 
         assertEquals(0, testEmployeeManager.getAvailableEmployees().size());
-        assertEquals(0, testEmployeeManager.getAvailableEmployeesData().size());
-
-        assertEquals(1, testEmployeeManager.getUnavailableEmployees().size());
-        assertEquals(1, testEmployeeManager.getUnavailableEmployeesData().size());
-        //remove hardcode
-        assertEquals(3, testEmployeeManager.getUnavailableEmployees().get(0).getDaysLeft());
-
     }
 
-    //not completed
+    //remove hardcode
     @Test
-    public void sendToTrainingTest() {
+    public void sendToTraining_IndexInBounds_AvailableEmployeesDataListHasNoElements() {
         testEmployeeManager = new EmployeeManager();
         testEmployeeManager.add(testEmployee);
         testEmployeeManager.sendToTraining(0);
+        testEmployee = testEmployeeManager.getUnavailableEmployees().get(0);
 
-        assertNotNull(testEmployeeManager.getAllEmployees());
-
-        assertEquals(0, testEmployeeManager.getAvailableEmployees().size());
         assertEquals(0, testEmployeeManager.getAvailableEmployeesData().size());
+    }
+
+    //remove hardcode
+    @Test
+    public void sendToTraining_IndexInBounds_UnavailableEmployeesListHasOneElements() {
+        testEmployeeManager = new EmployeeManager();
+        testEmployeeManager.add(testEmployee);
+        testEmployeeManager.sendToTraining(0);
+        testEmployee = testEmployeeManager.getUnavailableEmployees().get(0);
 
         assertEquals(1, testEmployeeManager.getUnavailableEmployees().size());
+    }
+
+    //remove hardcode
+    @Test
+    public void sendToTraining_IndexInBounds_UnavailableEmployeesDataListHasOneElements() {
+        testEmployeeManager = new EmployeeManager();
+        testEmployeeManager.add(testEmployee);
+        testEmployeeManager.sendToTraining(0);
+        testEmployee = testEmployeeManager.getUnavailableEmployees().get(0);
+
         assertEquals(1, testEmployeeManager.getUnavailableEmployeesData().size());
     }
 
+    //remove hardcode
     @Test
-    public void removeTest() {
+    public void sendToTraining_IndexInBounds_DaysLeftChanges() {
+        testEmployeeManager = new EmployeeManager();
+        testEmployeeManager.add(testEmployee);
+        testEmployeeManager.sendToTraining(0);
+        testEmployee = testEmployeeManager.getUnavailableEmployees().get(0);
+
+        assertEquals(3, testEmployeeManager.getUnavailableEmployees().get(0).getDaysLeft());
+    }
+
+    @Test
+    public void updateTraining_IndexInBounds_DaysLeftChanges() {
+        testEmployeeManager = new EmployeeManager();
+        testEmployeeManager.add(testEmployee);
+        testEmployeeManager.sendToTraining(0);
+        testEmployeeManager.updateTraining();
+
+        assertEquals(2, testEmployeeManager.getUnavailableEmployees().get(0).getDaysLeft());
+    }
+
+    @Test
+    public void removeTest_IndexInBounds_AllEmployeesListHasNoElements() {
         testEmployeeManager = new EmployeeManager();
         testEmployeeManager.add(testEmployee);
         testEmployeeManager.remove(0);
-        assertNotNull(testEmployeeManager.getAllEmployees());
-        assertNotNull(testEmployeeManager.getAvailableEmployees());
-        assertNotNull(testEmployeeManager.getAvailableEmployeesData());
+        assertEquals(0,testEmployeeManager.getAllEmployees().size());
+    }
+
+    @Test
+    public void removeTest_IndexInBounds_AvailableEmployeesListHasNoElements() {
+        testEmployeeManager = new EmployeeManager();
+        testEmployeeManager.add(testEmployee);
+        testEmployeeManager.remove(0);
+        assertEquals(0,testEmployeeManager.getAvailableEmployees().size());
+    }
+
+    @Test
+    public void removeTest_IndexInBounds_AvailableEmployeesDataListHasNoElements() {
+        testEmployeeManager = new EmployeeManager();
+        testEmployeeManager.add(testEmployee);
+        testEmployeeManager.remove(0);
+        assertEquals(0,testEmployeeManager.getAvailableEmployeesData().size());
     }
 
     @Test
     public void getAllEmployeesTest() {
         testEmployeeManager = new EmployeeManager();
         testEmployeeManager.add(testEmployee);
-        assertNotNull(testEmployeeManager.getAllEmployees());
-        assertEquals(1, testEmployeeManager.getAllEmployees().size());
-
-        assertNotNull(testEmployeeManager.getAllEmployees());
         assertEquals(1, testEmployeeManager.getAllEmployees().size());
     }
 
@@ -95,7 +156,6 @@ public class EmployeeManagerTest {
         testEmployeeManager = new EmployeeManager();
         testEmployeeManager.add(testEmployee);
 
-        assertNotNull(testEmployeeManager.getAvailableEmployees());
         assertEquals(1, testEmployeeManager.getAvailableEmployees().size());
     }
 
@@ -105,7 +165,6 @@ public class EmployeeManagerTest {
         testEmployeeManager.add(testEmployee);
         testEmployeeManager.sendToTraining(0);
 
-        assertNotNull(testEmployeeManager.getUnavailableEmployees());
         assertEquals(1, testEmployeeManager.getUnavailableEmployees().size());
     }
 
@@ -114,7 +173,6 @@ public class EmployeeManagerTest {
         testEmployeeManager = new EmployeeManager();
         testEmployeeManager.add(testEmployee);
 
-        assertNotNull(testEmployeeManager.getAvailableEmployeesData());
         assertEquals(1, testEmployeeManager.getAvailableEmployeesData().size());
     }
 
@@ -124,62 +182,95 @@ public class EmployeeManagerTest {
         testEmployeeManager.add(testEmployee);
         testEmployeeManager.sendToTraining(0);
 
-        assertNotNull(testEmployeeManager.getUnavailableEmployeesData());
         assertEquals(1, testEmployeeManager.getUnavailableEmployeesData().size());
     }
 
     @Test
-    public void updateTrainingTest() {
+    public void updateTraining_NoDaysLeft_UnavailableEmployeesListHasNoElements() {
         testEmployeeManager = new EmployeeManager();
         testEmployeeManager.add(testEmployee);
         testEmployeeManager.sendToTraining(0);
         testEmployeeManager.updateTraining();
 
-        assertEquals(2, testEmployeeManager.getUnavailableEmployees().get(0).getDaysLeft());
-
-    }
-
-    @Test
-    public void updateTraining_NoDaysLeft() {
-        testEmployeeManager = new EmployeeManager();
-        testEmployeeManager.add(testEmployee);
-        testEmployeeManager.sendToTraining(0);
-        testEmployeeManager.updateTraining();
-        testEmployeeManager.updateTraining();
-        testEmployeeManager.updateTraining();
-        testEmployeeManager.updateTraining();
+        while(testEmployeeManager.getAllEmployees().get(0).getDaysLeft()>0){
+            testEmployeeManager.updateTraining();
+        }
 
         assertEquals(0, testEmployeeManager.getUnavailableEmployees().size());
-        assertEquals(0, testEmployeeManager.getUnavailableEmployeesData().size());
-
-        assertEquals(1, testEmployeeManager.getAvailableEmployees().size());
-        assertEquals(1, testEmployeeManager.getAvailableEmployeesData().size());
-
     }
 
     @Test
-    public void addSkillTest() {
+    public void updateTraining_NoDaysLeft_UnavailableEmployeesDataListHasNoElements() {
+        testEmployeeManager = new EmployeeManager();
+        testEmployeeManager.add(testEmployee);
+        testEmployeeManager.sendToTraining(0);
+        testEmployeeManager.updateTraining();
+
+        while(testEmployeeManager.getAllEmployees().get(0).getDaysLeft()>0){
+            testEmployeeManager.updateTraining();
+        }
+
+        assertEquals(0, testEmployeeManager.getUnavailableEmployeesData().size());
+    }
+
+    @Test
+    public void updateTraining_NoDaysLeft_AvailableEmployeesListHasNoElements() {
+        testEmployeeManager = new EmployeeManager();
+        testEmployeeManager.add(testEmployee);
+        testEmployeeManager.sendToTraining(0);
+        testEmployeeManager.updateTraining();
+
+        while(testEmployeeManager.getAllEmployees().get(0).getDaysLeft()>0){
+            testEmployeeManager.updateTraining();
+        }
+
+        assertEquals(1, testEmployeeManager.getAvailableEmployees().size());
+    }
+
+    @Test
+    public void updateTraining_NoDaysLeft_AvailableEmployeesDataListHasNoElements() {
+        testEmployeeManager = new EmployeeManager();
+        testEmployeeManager.add(testEmployee);
+        testEmployeeManager.sendToTraining(0);
+        testEmployeeManager.updateTraining();
+
+        while(testEmployeeManager.getAllEmployees().get(0).getDaysLeft()>0){
+            testEmployeeManager.updateTraining();
+        }
+
+        assertEquals(1, testEmployeeManager.getAvailableEmployeesData().size());
+    }
+
+    @Test
+    public void addSkill_EmployeeExist_SkillChanges() {
         testEmployeeManager = new EmployeeManager();
         testEmployeeManager.add(testEmployee);
         testEmployeeManager.addSkill();
 
         assertEquals(2, testEmployeeManager.getAvailableEmployees().get(0).getSkill());
+    }
+
+    @Test
+    public void addSkill_EmployeeExist_EmployeeDataChanges() {
+        testEmployeeManager = new EmployeeManager();
+        testEmployeeManager.add(testEmployee);
+        testEmployeeManager.addSkill();
+
         assertEquals("Skill: 2", testEmployeeManager.getAvailableEmployeesData().get(0).get("Skill"));
     }
 
     @Test
-    public void changeSalaryTest() {
+    public void changeSalary_CorrectPositionAndValue_SalaryChanges() {
         testEmployeeManager = new EmployeeManager();
         testEmployeeManager.add(testEmployee);
-        testEmployeeManager.changeSalary(0,100);
+        testEmployeeManager.changeSalary(0, 100);
 
         //remove hardcode
         assertEquals(102, testEmployeeManager.getAvailableEmployees().get(0).getSalary());
-
     }
 
     @Test
-    public void changeSalaryTest2() {
+    public void changeSalary_IncorrectPositionAndValue_SalaryDoesNotChange() {
         testEmployee.setSalary(10);
         testEmployeeManager = new EmployeeManager();
         testEmployeeManager.add(testEmployee);
@@ -189,6 +280,5 @@ public class EmployeeManagerTest {
         assertEquals(10, testEmployeeManager.getAvailableEmployees().get(0).getSalary());
 
     }
-
 
 }
